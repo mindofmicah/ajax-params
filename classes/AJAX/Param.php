0cff81fe0b0c13fdbd286fc641b5e8ee62e1c2bf
michael eschbacher <michaeleschbacher@gmail.com>
2012-12-09 14:54:26 -0600

    Created  a 'default' 'to' approach for keys that are not required

--- classes/AJAX/Param.php
+++ classes/AJAX/Param.php
@@ -7,13 +7,20 @@
  */
 class AJAX_Param
 {
-
+    protected $value;
     static protected $pool = array();
 
-    public function __construct()
+    public function __construct($value = null)
     {
-        
+        $this->value = $value;
     }
+    public function setValue($value)
+            {
+        $this->value = $value;
+            }
+            public function getValue(){
+                return $this->value;
+            }
     static public function fromKey($key, $defaultValue = null)
     {
         if (array_key_exists($key, self::$pool)) {
@@ -36,5 +43,23 @@ class AJAX_Param
     {
         
     }
-
+    
+    static public function defaults($key)
+    {
+        if (!empty(self::$pool[$key])) {
+            return new AJAX_Param(self::$pool[$key]);
+        }
+        return new AJAX_Param;
+    }
+    public function to($value)
+    {
+        if (is_null($this->value)) {
+            $this->value = $value;
+        }
+        return $this;
+    }
+    public function __toString()
+    {
+        return $this->value;
+    }
 }
